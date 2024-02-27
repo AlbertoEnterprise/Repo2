ad12e37b4dced1f33d3079d8fae0921d265821f5
albertoldm <126006995+albertoldm@users.noreply.github.com>
2024-02-27 12:07:01 +0100

    Add files via upload

---
+++ slqin.py
@@ -0,0 +1,22 @@
+import sqlite3
+
+def get_user(username):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    
+    # Vulnerabilidad de inyección de SQL
+    cursor.execute("SELECT * FROM users WHERE username = '" + username + "'")
+    
+    user = cursor.fetchone()
+    conn.close()
+    
+    return user
+
+if __name__ == "__main__":
+    username = input("Ingrese el nombre de usuario: ")
+    user = get_user(username)
+    
+    if user:
+        print("¡Bienvenido, {}!".format(user[1]))
+    else:
+        print("Usuario no encontrado.")
