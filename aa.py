022b8b3e4504eda8a430dd97b575e576c2ba58e0
albertoldm <126006995+albertoldm@users.noreply.github.com>
2024-02-27 13:52:21 +0100

    Create aa.py

---
+++ aa.py
@@ -0,0 +1,8 @@
+import warnings
+
+def mi_funcion():
+    # Simulación de un problema en el código
+    warnings.warn("¡Atención! Este código tiene un problema de seguridad.", UserWarning)
+
+# Llamamos a la función
+mi_funcion()
